922c1e2f9b30d00a47c7de27490d9972a48b25c5
Saifa Decha <Saifadeacha@gmail.com>
2026-04-09 15:24:11 +0700

    project

--- migrations/env.py
+++ migrations/env.py
@@ -1,7 +1,13 @@
 from logging.config import fileConfig
+from pathlib import Path
+import sys
 
 from alembic import context
 from sqlalchemy import engine_from_config, pool
+
+PROJECT_ROOT = Path(__file__).resolve().parents[1]
+if str(PROJECT_ROOT) not in sys.path:
+    sys.path.insert(0, str(PROJECT_ROOT))
 
 from app.config import get_settings
 from app.db.base import Base
